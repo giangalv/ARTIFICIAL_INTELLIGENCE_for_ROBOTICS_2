a5aaccfc51eee7651e7c6b28f24ae32c3e54ffc6
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2023-07-08 14:33:33 +0200

    Add files via upload

---
+++ 2_ASSIGNMENT_AIRO2/visits_module/build/CMakeFiles/VisitMain.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gianluca/Documents/GitHub/AIRO2_SecondAssignment/visits_module/src/main.cpp" "CMakeFiles/VisitMain.dir/main.cpp.o" "gcc" "CMakeFiles/VisitMain.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/gianluca/Documents/GitHub/AIRO2_SecondAssignment/visits_module/build/CMakeFiles/Visits.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
